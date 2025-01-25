b1cd209f4bc1d6b4edcb0f8a727cb34fbc79dec1
Hemasundhar <116507961+Hemasundhar001@users.noreply.github.com>
2025-01-25 17:25:55 +0530

    user room booking details ( page 9)

---
+++ bookeddetails.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace hotel_registrion_project
+{
+    public partial class bookeddetails : System.Web.UI.Page
+    {
+        private void GetData()
+        {
+            SqlConnection con = new SqlConnection("data source=.;Database=hotel registration;integrated security = yes");
+            SqlCommand cmd = new SqlCommand("select * from roombook", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            GridView1.DataSource = ds.Tables[0];
+            GridView1.DataBind();
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetData();
+            }
+
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            GridViewRow rows = GridView1.Rows[e.RowIndex];
+            Label lbl = (Label)rows.FindControl("Label1");
+            SqlConnection con = new SqlConnection("data source=HEMASUNDHAR1;Database=hotel registration;integrated security = yes");
+            con.Open();
+            var query = "delete from roombook where username ='" + lbl.Text + "'";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            GetData();
+
+        }
+    }
+}
